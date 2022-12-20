d7172ef9fa2593898c2d962173d4c8a1c6136042
ritaclobo <105011894+ritaclobo@users.noreply.github.com>
2022-12-20 22:18:52 +0000

    Update streamlit_page.py

--- streamlit_page.py
+++ streamlit_page.py
@@ -12,11 +12,12 @@ st.title("Projeto de AAIB")
 
 st.subheader("Distinção entre 4 classes relativas a movimentos em ballet")
 
-st.write("O objetivo deste projeto foi a utilização do Raspberry PI para adquiri dados de movimento, mais concretamente de acelerómetro e giroscópio, classficiando-os mais tarde com o uso de Machine Learning.")
-st.write("Desta forma, nesta página pode-se visualizar os dados adquiridos num gráfico, e está também disponível a informação relativamente à classe do movimento.")
-st.write("Considerou-se então: D1, D2")
-
 with st.sidebar:
+    st.write("O objetivo deste projeto foi a utilização do Raspberry PI para adquirir dados de movimento, mais concretamente de acelerómetro e giroscópio, classificando-os mais tarde com o uso de Machine Learning.")
+    st.write("Desta forma, nesta página pode-se visualizar os dados adquiridos em dois gráficos, estando também disponível a informação relativamente à classe do movimento.")
+    st.write("Considerou-se 4 posições diferentes: D1 (desiquilíbrio para a frente), D2 (desiquilíbrio para trás), Eq (Equilíbrio), Ex (classe de exclusão).")
+    st.write("Estas vão ser equivalentes às classes: 1, 2, 3 e 0, respetivamente.")
+    st.write("")
     st.write("Escolher o que se quer vizualizar:")
     checkbox_one = st.checkbox("Acelerómetro")
     checkbox_two = st.checkbox("Giroscópio")
@@ -31,13 +32,17 @@ if start_button.button("Start", key='start', type="secondary", disabled=False):
 #Dataframe
 df = pd.read_csv("outro_teste2.csv", header=None)
 df.columns = ["Timestamp", "Accx", "Accy", "Accz", "Gyrox", "Gyroy", "Gyroz", "Classe"]
-data_Sub1 = data_Sub.drop("Timestamp", axis=1)
+data_Sub1 = df.drop("Timestamp", axis=1)
 
 #DataFrame com dados do Acelerómetro
 data_Acc = data_Sub1[["Accx","Accy","Accz"]]
 
 #DataFrame com dados do Giroscópio
 data_Gyr = data_Sub1[["Gyrox","Gyroy","Gyroz"]]
+
+#f0=data_sub1["Classe"][0]
+f0=2
+st.write("A classe é : ", f"{f0}")
 
 #Acc
 if checkbox_one:
@@ -46,7 +51,3 @@ if checkbox_one:
 #Gyr
 if checkbox_two:
     st.line_chart(data_Gyr)
-
-#f0=data_sub1["Classe"][0]
-f0=2
-st.write("A classe é : ", f"{f0}")
